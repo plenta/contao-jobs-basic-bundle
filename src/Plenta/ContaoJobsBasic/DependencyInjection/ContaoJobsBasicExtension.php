c38690db9baa481c835579c66ba5adacfd7b2730
Christoph Werner <christopwerner@gmx.at>
2020-02-04 11:09:15 +0100

    phpstan

---
+++ src/Plenta/ContaoJobsBasic/DependencyInjection/ContaoJobsBasicExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Plenta Jobs Basic Bundle for Contao CMS
+ *
+ * @copyright     Copyright (c) 2020, Christian Barkowsky & Christoph Werner
+ * @author        Christian Barkowsky <https://plenta.io>
+ * @author        Christoph Werner <https://plenta.io>
+ * @link          https://plenta.io
+ * @license       proprietary
+ */
+
+namespace Plenta\ContaoJobsBasic\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class ContaoJobsBasicExtension extends Extension
+{
+    /**
+     * @param array            $configs
+     * @param ContainerBuilder $container
+     *
+     * @throws \Exception
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
