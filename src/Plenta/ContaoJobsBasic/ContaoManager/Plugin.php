9245d35b50cd48c1801505c9b5e2f1d723e74471
Christoph Werner <christopwerner@gmx.at>
2020-02-04 10:33:29 +0100

    init

---
+++ src/Plenta/ContaoJobsBasic/ContaoManager/Plugin.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Plenta Jobs Basic Bundle for Contao CMS
+ *
+ * @copyright     Copyright (c) 2020, Christian Barkowsky & Christoph Werner
+ * @author        Christian Barkowsky <https://plenta.io>
+ * @author        Christoph Werner <https://plenta.io>
+ * @link          https://plenta.io
+ * @license       proprietary
+ */
+
+namespace Plenta\ContaoJobsBasic;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Plenta\ContaoJobsBasic\PlentaContaoJobsBasicBundle;
+
+/**
+ * Class ContaoManagerPlugin.
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(PlentaContaoJobsBasicBundle::class)
+                ->setLoadAfter([
+                    ContaoCoreBundle::class,
+                ]),
+        ];
+    }
+}
---
+++ src/Plenta/ContaoJobsBasic/PlentaContaoJobsBasicBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Plenta Jobs Basic Bundle for Contao CMS
+ *
+ * @copyright     Copyright (c) 2020, Christian Barkowsky & Christoph Werner
+ * @author        Christian Barkowsky <https://plenta.io>
+ * @author        Christoph Werner <https://plenta.io>
+ * @link          https://plenta.io
+ * @license       proprietary
+ */
+
+namespace Plenta\ContaoJobsBasic;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Configures the bundle.
+ */
+class PlentaContaoJobsBasicBundle extends Bundle
+{
+}
