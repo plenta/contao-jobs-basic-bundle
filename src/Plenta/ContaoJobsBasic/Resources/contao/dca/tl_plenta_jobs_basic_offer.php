4be9130e91b02fafe339632b900645983dc272f3
Christian Barkowsky <hallo@brkwsky.de>
2021-11-05 10:16:55 +0100

    #5 #8

--- src/Plenta/ContaoJobsBasic/Resources/contao/dca/tl_plenta_jobs_basic_offer.php
+++ src/Plenta/ContaoJobsBasic/Resources/contao/dca/tl_plenta_jobs_basic_offer.php
@@ -42,6 +42,21 @@ $GLOBALS['TL_DCA']['tl_plenta_jobs_basic_offer'] = [
                 'href' => 'act=edit',
                 'icon' => 'header.svg',
             ],
+            'delete' => [
+                'href' => 'act=delete',
+                'icon' => 'delete.svg',
+                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+            ],
+            'toggle' => [
+                'icon' => 'visible.svg',
+                'attributes' => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
+                //'button_callback' => ['tl_news', 'toggleIcon'],
+                'showInHeader' => true,
+            ],
+            'show' => [
+                'href' => 'act=show',
+                'icon' => 'show.svg',
+            ],
         ],
     ],
 
