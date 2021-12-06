1946876f99824481d2c8d9a5b9b44a4d2551fb01
Christian Barkowsky <hallo@brkwsky.de>
2021-12-06 13:42:45 +0100

    #4

--- src/Plenta/ContaoJobsBasic/Controller/Contao/FrontendModule/JobOfferFilterController.php
+++ src/Plenta/ContaoJobsBasic/Controller/Contao/FrontendModule/JobOfferFilterController.php
@@ -14,6 +14,8 @@ namespace Plenta\ContaoJobsBasic\Controller\Contao\FrontendModule;
 
 use Contao\Template;
 use Contao\ModuleModel;
+use Contao\FormCheckBox;
+use Plenta\ContaoJobsBasic\Helper\EmploymentType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Contao\CoreBundle\ServiceAnnotation\FrontendModule;
@@ -22,15 +24,58 @@ use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController
 /**
  * @FrontendModule("plenta_jobs_basic_filter_reader",
  *   category="plentaJobsBasic",
- *   template="mod_plenta_jobs_basic_filter_reader",
+ *   template="mod_plenta_jobs_basic_filter",
  *   renderer="forward"
  * )
  */
 class JobOfferFilterController extends AbstractFrontendModuleController
 {
+    protected EmploymentType $employmentTypeHelper;
+
+    public function __construct(
+        EmploymentType $employmentTypeHelper
+    ) {
+        $this->employmentTypeHelper = $employmentTypeHelper;
+    }
 
     protected function getResponse(Template $template, ModuleModel $model, Request $request): ?Response
     {
+        $model = new ModuleModel();
+        $model->tstamp = time();
+
+        $checkbox = new FormCheckBox($model);
+        $checkbox->options = $this->getTypes();
+        $checkbox->eval = ['multiple'=>true];
+
+        $template->types = $checkbox->generate();
+
         return $template->getResponse();
+    }
+
+    public function getTypes(): ?array
+    {
+        $options = [];
+        $employmentTypes = [];
+        $employmentTypeHelper = $this->employmentTypeHelper;
+
+        foreach ($employmentTypeHelper->getEmploymentTypes() as $employmentType) {
+            $employmentTypes[$employmentType] = $employmentTypeHelper->getEmploymentTypeName($employmentType);
+        }
+
+        if (array_is_assoc($employmentTypes)) {
+            foreach ($employmentTypes as $k => $v) {
+                if (isset($v['label'])) {
+                    $options[] = $v;
+                } else {
+                    $options[] = [
+                        'label' => $v,
+                        'value' => $k,
+                        'group' => '1',
+                    ];
+                }
+            }
+        }
+
+        return $options;
     }
 }
