c5b85f6c21749edd7f0fdfc01720c0b226b7746a
veronikaplenta <veronika@plenta.io>
2022-04-04 10:38:18 +0200

    #19

--- src/Plenta/ContaoJobsBasic/EventListener/Contao/DCA/JobOfferFields.php
+++ src/Plenta/ContaoJobsBasic/EventListener/Contao/DCA/JobOfferFields.php
@@ -1,5 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Plenta Jobs Basic Bundle for Contao Open Source CMS
+ *
+ * @copyright     Copyright (c) 2022, Plenta.io
+ * @author        Plenta.io <https://plenta.io>
+ * @link          https://github.com/plenta/
+ */
+
 namespace Plenta\ContaoJobsBasic\EventListener\Contao\DCA;
 
 class JobOfferFields
